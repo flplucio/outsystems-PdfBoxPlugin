9114dfc4e7a45eedb1797c79926beed0ace6b489
flplucio <45046100+flplucio@users.noreply.github.com>
2020-07-24 09:37:50 +0100

    Java code dependencies

---
+++ src/android/PdfBoxPlugin.java
@@ -0,0 +1,13 @@
+package com.flplucio.cordova.plugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Set;
+import java.util.List;
+
--- src/android/outsystems-PdfBoxPlugin.java
+++
@@ -1,1 +0,0 @@
-package com.flplucio.cordova.plugin.PdfBoxPlugin;
